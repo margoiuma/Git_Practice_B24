ad2415a71d95789abbbdb30a64a8ae104216c391
margoiuma <margo.ritaechh@gmail.com>
2021-10-03 15:51:52 -0500

    updated hello world class

--- src/tests/HelloWorldTest.java
+++ src/tests/HelloWorldTest.java
@@ -3,5 +3,7 @@ package tests;
 public class HelloWorldTest {
     public static void main(String[] args){
         System.out.println("hello world");
+
+        System.out.println("one more hello world");
     }
 }
