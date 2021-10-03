fa5f93cb033bef4cc26a0b37f17b6b1a6fbea992
margoiuma <margo.ritaechh@gmail.com>
2021-10-03 15:19:54 -0500

    updated car file in master branch

--- src/Setup/Car.java
+++ src/Setup/Car.java
@@ -1,14 +1,14 @@
 package Setup;
 
 public class Car {
-<<<<<<< HEAD
+
 
     //this is car class from master branch
 
     //we will have conflict  because there is different text
-=======
+
     //this is a car from james branch
 
     //more text
->>>>>>> james
+
 }
